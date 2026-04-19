1f9223dbc946676f29900398c5c3a7d3b0b153e0
Rewanabegun <238w1a1234@vrsec.ac.in>
2026-04-19 17:13:00 +0530

    Smart City Dashboard Project

--- server/src/server.js
+++ server/src/server.js
@@ -52,7 +52,7 @@ io.on("connection", (socket) => {
 });
 
 // ================= START SERVER =================
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5001;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT} 🚀`);
