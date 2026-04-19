28faa516d3db577be1a18fb255c0212cf720792b
Rewanabegun <238w1a1234@vrsec.ac.in>
2026-04-19 19:58:14 +0530

    time

--- client/src/components/TrafficChart.jsx
+++ client/src/components/TrafficChart.jsx
@@ -1,18 +1,33 @@
-import React from "react";
-import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
+import {
+  LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer
+} from "recharts";
 
 const TrafficChart = ({ data }) => {
   return (
-    <div style={{ width: "100%", height: 300 }}>
-      <ResponsiveContainer>
-        <LineChart data={data}>
-          <XAxis dataKey="time" />
-          <YAxis />
-          <Tooltip />
-          <Line type="monotone" dataKey="density" stroke="#8884d8" />
-        </LineChart>
-      </ResponsiveContainer>
-    </div>
+    <ResponsiveContainer width="100%" height={250}>
+      <LineChart data={data}>
+
+        <XAxis
+          dataKey="time"
+          stroke="#ccc"
+          tickFormatter={(time) =>
+            new Date(time).toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit"
+            })
+          }
+        />
+
+        <YAxis stroke="#ccc" />
+        <Tooltip
+          labelFormatter={(time) =>
+            new Date(time).toLocaleTimeString()
+          }
+        />
+
+        <Line type="monotone" dataKey="density" stroke="#22c55e" />
+      </LineChart>
+    </ResponsiveContainer>
   );
 };
 
